640856d8978f82d0b23560802c599f1ee27a1480
wickerman2019 <sverige@i.ua>
2023-01-09 03:53:43 +0200

    Fix bug with division

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        System.out.println(toBeDivided / 5);
+        System.out.println((float)toBeDivided / 5);
     }
 
 }
