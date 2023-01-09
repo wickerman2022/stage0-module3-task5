4c6a208d122761c2da930dd69f16b3ec99df7d8a
wickerman2019 <sverige@i.ua>
2023-01-09 03:47:20 +0200

    Operators

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.printf("%d%d%d\n",first,second,third);
     }
 }
